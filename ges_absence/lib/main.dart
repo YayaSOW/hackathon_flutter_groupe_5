dc216f0a23cf4d47c30999cfa203a6dccc4fb7b1
Yaya SOW <yaya.sowzone@gmail.com.com>
2025-05-30 21:36:34 +0000

    factorisation-routes

--- ges_absence/lib/main.dart
+++ ges_absence/lib/main.dart
@@ -1,15 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:ges_absence/app/bindings/global_binding.dart';
-import 'package:ges_absence/app/bindings/etudiant_binding.dart';
-import 'package:ges_absence/app/bindings/vigile_binding.dart';
-import 'package:ges_absence/app/modules/etudiant/views/absence_view.dart';
-import 'package:ges_absence/app/modules/etudiant/views/cours_view.dart';
-import 'package:ges_absence/app/modules/etudiant/views/home_view.dart';
-import 'package:ges_absence/app/modules/etudiant/views/justification_view.dart';
-import 'package:ges_absence/app/modules/auth/views/login_view.dart';
-import 'package:ges_absence/app/modules/etudiant/views/retards_view.dart';
-import 'package:ges_absence/app/modules/vigile/views/home_view.dart';
-import 'package:ges_absence/app/routes/app_routes.dart';
+import 'package:ges_absence/app/routes/app_pages.dart';
 import 'package:ges_absence/theme/colors.dart';
 import 'package:get/get.dart';
 
@@ -29,46 +20,11 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSwatch().copyWith(
           secondary: AppColors.secondaryColor,
         ),
-        // fontFamily: 'Roboto',
+        fontFamily: 'Roboto',
       ),
       initialBinding: GlobalBinding(),
-      initialRoute: AppRoutes.LOGIN,
-      getPages: [
-        GetPage(
-          name: AppRoutes.LOGIN,
-          page: () => LoginView(),
-          binding: EtudiantBinding(),
-        ),
-        GetPage(
-          name: AppRoutes.Etudiant_HOME,
-          page: () => HomeView(),
-          // binding: VigileBinding(),
-        ),
-        GetPage(
-          name: AppRoutes.COURS,
-          page: () => CoursView(),
-          binding: EtudiantBinding(),
-        ),
-        GetPage(name: AppRoutes.ABSENCES, 
-        page: () => AbsencesView(),
-        binding: EtudiantBinding(),
-        ),
-        GetPage(
-          name: AppRoutes.JUSTIFICATION,
-          page: () => JustificationView(absence: Get.arguments),
-          binding: EtudiantBinding(),
-        ),
-        GetPage(
-          name: AppRoutes.RETARDS,
-          page: () => RetardsView(),
-          binding: EtudiantBinding(),
-        ),
-        GetPage(
-          name: AppRoutes.Vigile_HOME,
-          page: () => VigileHomeView(),
-          binding: VigileBinding(), 
-        ),
-      ],
+      initialRoute: AppPages.initial, 
+      getPages: AppPages.pages, 
       debugShowCheckedModeBanner: false,
     );
   }
